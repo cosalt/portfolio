75c9cee707b21c479a6ce522ae060843a943f360
Nawin Ray Martin <thaimartin369@gmail.com>
2024-11-26 21:49:14 +0700

    Update deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import github from '@astrojs/github-pages';
 
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://cosalt.github.io',
-  base: '/github-projects-portfolio',
+  site: 'https://cosalt.github.io/portfolio',
+  base: '/portfolio', // Update base to match your repository name
+  output: 'static',
+  adapter: github(),
 });
